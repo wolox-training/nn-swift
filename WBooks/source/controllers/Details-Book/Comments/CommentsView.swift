8588dba495462c321674d9222d6d313aebf4fc04
Noelia <noelia.nieres@accenture.com>
2021-09-03 16:42:11 -0300

    Se modifico vista de comentarios

--- WBooks/source/controllers/Details-Book/Comments/CommentsView.swift
+++ WBooks/source/controllers/Details-Book/Comments/CommentsView.swift
@@ -15,6 +15,22 @@ protocol CommentsViewProtocol: UIView {
 class CommentsView: NibView {
     @IBOutlet weak var commentsTable: UITableView!
     
+    @IBOutlet weak var borderView: UIView!{
+        didSet {
+            borderView.layer.cornerRadius = 5
+            borderView.backgroundColor = .white
+            borderView.layer.cornerRadius = 20
+            borderView.layer.borderWidth = 1
+            borderView.layer.borderColor = #colorLiteral(red: 0.9999960065, green: 1, blue: 1, alpha: 1)
+            borderView.clipsToBounds = true
+        }
+    }
+    
+    @IBOutlet var backgroundView: UIView!{
+        didSet {
+            backgroundView.backgroundColor = .backgroundPolar()
+        }
+    }
 }
 
 
